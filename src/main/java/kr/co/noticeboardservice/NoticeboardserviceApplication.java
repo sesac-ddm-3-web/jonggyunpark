f945261ad5eca3706011a9f0b78e5f3ac5b92160
gyunnybot <idjkda4034@gmail.com>
2025-11-30 19:45:14 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/kr/co/noticeboardservice/NoticeboardserviceApplication.java
@@ -0,0 +1,13 @@
+package kr.co.noticeboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeboardserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeboardserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/noticeboardservice/NoticeboardserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.noticeboardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeboardserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
